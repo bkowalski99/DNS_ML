3a25a2af1bbe4bf095f1dfd960af30d871f7ae92
Ben Kowalski <50568067+bkowalski99@users.noreply.github.com>
2020-08-14 11:48:55 -0400

    Update normalizeInputData8By8.py

--- normalizeInputData8By8.py
+++ normalizeInputData8By8.py
@@ -2,16 +2,10 @@ import numpy as np
 
 print('starting')
 # loads in data from the saved numpy files in dataInput
-loadedinputs = np.load('inputs.npy')
-loadedtargets = np.load('targets.npy')
 inputs_8x8 = np.load('8X8_inputs.npy')
 targets_8x8 = np.load('8X8_targets.npy')
 # file to save information for when we convert back
 
-
-# TODO: Saved max and min to reset data after NN runs
-loadedinputs = loadedinputs.reshape(3072, 32, 32)
-loadedtargets = loadedtargets.reshape(3072, 32, 32)
 inputs_8x8 = inputs_8x8.reshape(49152, 8, 8)
 targets_8x8 = targets_8x8.reshape(49152, 8, 8)
 
@@ -26,7 +20,6 @@ for i in range(len(inputs_8x8)):
         print('Run:', str(i), ' max:', np.max(inputs_8x8[i]), ' min:', np.min(inputs_8x8[i]), file=file)
         inputs_8x8[i] = (inputs_8x8[i] - np.min(inputs_8x8[i])) / (np.max(inputs_8x8[i]) - np.min(inputs_8x8[i]))
 
-
 file.close()
 
 file = open('targetsConversion8x8.txt', 'w')
@@ -37,11 +30,8 @@ for i in range(len(targets_8x8)):
         print('Run:', str(i), ' max:', np.max(targets_8x8[i]), ' min:', np.min(targets_8x8[i]), file=file)
         targets_8x8[i] = (targets_8x8[i] - np.min(targets_8x8[i])) / (np.max(targets_8x8[i]) - np.min(targets_8x8[i]))
 
-
 file.close()
 
-np.save('normedInputs', loadedinputs)
-np.save('normedTargets', loadedtargets)
 np.save('8x8NormedInputs', inputs_8x8)
 np.save('8x8NormedTargets', targets_8x8)
 print('done')
