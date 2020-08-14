c30ac7a66d772a416f37fda68abd18221dc053cb
Ben Kowalski <50568067+bkowalski99@users.noreply.github.com>
2020-08-14 11:46:56 -0400

    Create normalizeInputData8By8.py

---
+++ normalizeInputData8By8.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+print('starting')
+# loads in data from the saved numpy files in dataInput
+loadedinputs = np.load('inputs.npy')
+loadedtargets = np.load('targets.npy')
+inputs_8x8 = np.load('8X8_inputs.npy')
+targets_8x8 = np.load('8X8_targets.npy')
+# file to save information for when we convert back
+
+
+# TODO: Saved max and min to reset data after NN runs
+loadedinputs = loadedinputs.reshape(3072, 32, 32)
+loadedtargets = loadedtargets.reshape(3072, 32, 32)
+inputs_8x8 = inputs_8x8.reshape(49152, 8, 8)
+targets_8x8 = targets_8x8.reshape(49152, 8, 8)
+
+# normalization uses x - min / max - min
+# this makes max possible value 1 and min possible value 0
+
+file = open('inputsConversion8x8.txt', 'w')
+
+print("Normalizing 8 by 8 inputs")
+for i in range(len(inputs_8x8)):
+    if np.max(inputs_8x8[i]) != np.min(inputs_8x8[i]):
+        print('Run:', str(i), ' max:', np.max(inputs_8x8[i]), ' min:', np.min(inputs_8x8[i]), file=file)
+        inputs_8x8[i] = (inputs_8x8[i] - np.min(inputs_8x8[i])) / (np.max(inputs_8x8[i]) - np.min(inputs_8x8[i]))
+
+
+file.close()
+
+file = open('targetsConversion8x8.txt', 'w')
+
+print("Normalizing 8 by 8 targets")
+for i in range(len(targets_8x8)):
+    if np.max(targets_8x8[i]) != np.min(targets_8x8[i]):
+        print('Run:', str(i), ' max:', np.max(targets_8x8[i]), ' min:', np.min(targets_8x8[i]), file=file)
+        targets_8x8[i] = (targets_8x8[i] - np.min(targets_8x8[i])) / (np.max(targets_8x8[i]) - np.min(targets_8x8[i]))
+
+
+file.close()
+
+np.save('normedInputs', loadedinputs)
+np.save('normedTargets', loadedtargets)
+np.save('8x8NormedInputs', inputs_8x8)
+np.save('8x8NormedTargets', targets_8x8)
+print('done')
